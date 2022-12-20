84c0852bdce97c90369941cfbf8099561edeecc2
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2022-12-20 10:58:54 -0600

    models.json correction

--- assets/models.js
+++ assets/models.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('#model-container').empty();
-	$.getJSON('assets/model.json', function(data) {
+	$.getJSON('assets/models.json', function(data) {
 		// JSON result in data variable
 		for (item in data) {
 			var name = data[item].name;
@@ -9,6 +9,19 @@ $(function() {
 			var releases = data[item].releases;
 			var tags = data[item].tags;
 			var IDize = name.replace(/[^A-Za-z0-9]/g,""); // Use to make sure its doesnt break html
+			
+			console.log(name, info_link, creator, releases, tags, IDize);
+			
+			for (i = 0; i < releases.length; i++) {
+				var release_entry = releases[i];
+				var r_name = release_entry.name;
+				var date = release_entry.release_date;
+				var download = release_entry.download_link;
+				var hash = release_entry.SHA256;
+				var desc = release_entry.description;
+				console.log(r_name, date, download, hash, desc);
+			} 
+			
 		}
 		new ClipboardJS('.btn');
 	})
