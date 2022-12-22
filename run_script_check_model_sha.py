5dae80cbd6e043db03f09273681e2ae522b34eaa
Yorzaren <55202832+Yorzaren@users.noreply.github.com>
2022-12-22 15:29:51 -0600

    ci test

--- run_script_check_model_sha.py
+++ run_script_check_model_sha.py
@@ -4,7 +4,7 @@ from check_model_sha import get_mixmatched_sha_output, has_mixmatched_sha, test_
 # https://stackoverflow.com/a/70123641
 env_file = os.getenv('GITHUB_ENV')
 
-arr = test_models_sha("assets/embeddings.json")
+arr = test_models_sha("assets/models.json")
 
 with open(env_file, "a") as myfile:
     myfile.write("COUNT_BAD_SHA="+str(has_mixmatched_sha(arr))+"\n")
